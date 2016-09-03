583b6834e905a8f9f2711da429c3ee9726bb80df
Srinivas Rao <Srinivas_Rao@avivacanada.com>
2016-09-03 11:30:32 -0400

    updated boot script

--- server/boot/create-sample-models.js
+++ server/boot/create-sample-models.js
@@ -42,11 +42,40 @@ module.exports = function(app) {
       if (err) return cb(err);
 
       app.models.CoffeeShop.create([
-        {name: 'Bel Cafe', city: 'Vancouver'},
-        {name: 'Three Bees Coffee House', city: 'San Mateo'},
-        {name: 'Caffe Artigiano', city: 'Vancouver'},
         {name: 'Personal Line Quoting', city: 'Toronto'},
-        {name: 'Personal Line Service', city: 'Toronto'}
+        {name: 'Personal Line Services', city: 'Toronto'},
+        {name: 'MyAviva - UK - Application', city: 'Toronto'},
+        {name: 'MyAviva - UK - UMT', city: 'Toronto'},
+        {name: 'MyAviva - UK - eSecurity', city: 'Toronto'},
+        {name: 'MyAviva - CA - Customer MDM Services', city: 'Toronto'},
+        {name: 'MyAviva - CA - Customer Management Service', city: 'Toronto'},
+        {name: 'MyAviva - CA - Policy  Management Service', city: 'Toronto'},
+        {name: 'MyAviva - CA - Service Interface', city: 'Toronto'},
+        {name: 'MyAviva - CA - Self Service', city: 'Toronto'},
+        {name: 'Marketing Tools - Analytics & Reporting', city: 'Toronto'},
+        {name: 'Marketing Tools - Campaign', city: 'Toronto'},
+        {name: 'Marketing Tools - CMS', city: 'Toronto'},
+        {name: 'Marketing Tools - Social', city: 'Toronto'},
+        {name: 'Marketing Tools - Target', city: 'Toronto'},
+        {name: 'Aviva Community Fund', city: 'Toronto'},
+        {name: 'Aviva Drive', city: 'Toronto'},
+        {name: 'Home Checker', city: 'Toronto'},
+        {name: 'Rating and Reviews', city: 'Toronto'},
+        {name: 'Commercial Lines Quoting & Services', city: 'Toronto'},
+        {name: 'AASP', city: 'Toronto'},
+        {name: 'Surety Quoting & Services', city: 'Toronto'},
+        {name: 'Bank Service', city: 'Toronto'},
+        {name: 'Broker Portal - Quoting & Service', city: 'Toronto'},
+        {name: 'Broker Management - ABC', city: 'Toronto'},
+        {name: 'Elite Quoting & Services', city: 'Toronto'},
+        {name: 'MDM', city: 'Toronto'},
+        {name: 'Special - Sites', city: 'Toronto'},
+        {name: 'Aviva.ca - Partner Websites', city: 'Toronto'},
+        {name: 'Aviva.ca - Brand & Marketing Websites', city: 'Toronto'},
+        {name: 'Micro-sites - Corporate Websites', city: 'Toronto'},
+        {name: 'Micro-sites - CSR Websites', city: 'Toronto'},
+        {name: 'Micro-sites - E-Commerce Websites', city: 'Toronto'},
+        {name: 'Group Direct Microsites', city: 'Toronto'}
       ], cb);
     });
   }
@@ -61,8 +90,8 @@ module.exports = function(app) {
       app.models.Review.create([
         {
           date: Date.now() - (DAY_IN_MILLISECONDS * 4),
-          rating: 5,
-          comments: 'A very good coffee shop.',
+          rating: 4,
+          comments: 'Return in investment was average.',
           publisherId: reviewers[0].id,
           coffeeShopId: coffeeShops[0].id
         },
@@ -71,21 +100,42 @@ module.exports = function(app) {
           rating: 5,
           comments: 'Quite pleasant.',
           publisherId: reviewers[1].id,
-          coffeeShopId: coffeeShops[0].id
+          coffeeShopId: coffeeShops[1].id
         },
         {
           date: Date.now() - (DAY_IN_MILLISECONDS * 2),
           rating: 4,
           comments: 'It was ok.',
           publisherId: reviewers[1].id,
-          coffeeShopId: coffeeShops[1].id
+          coffeeShopId: coffeeShops[2].id
+        },
+        {
+          date: Date.now() - (DAY_IN_MILLISECONDS),
+          rating: 2,
+          comments: 'Could be done better.',
+          publisherId: reviewers[2].id,
+          coffeeShopId: coffeeShops[4].id
         },
         {
           date: Date.now() - (DAY_IN_MILLISECONDS),
           rating: 4,
-          comments: 'I go here everyday.',
+          comments: 'I am not impressed',
+          publisherId: reviewers[0].id,
+          coffeeShopId: coffeeShops[5].id
+        },
+        {
+          date: Date.now() - (DAY_IN_MILLISECONDS),
+          rating: 1,
+          comments: 'Response time was very low.',
+          publisherId: reviewers[1].id,
+          coffeeShopId: coffeeShops[6].id
+        },
+        {
+          date: Date.now() - (DAY_IN_MILLISECONDS),
+          rating: 5,
+          comments: 'High performance.',
           publisherId: reviewers[2].id,
-          coffeeShopId: coffeeShops[2].id
+          coffeeShopId: coffeeShops[7].id
         }
       ], cb);
     });
